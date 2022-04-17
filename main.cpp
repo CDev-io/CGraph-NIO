2471a84b558ebb7a3c6c5a3230f290c429473bf5
ChunelFeng <chunel@foxmail.com>
2022-04-17 22:27:30 +0800

    [add] 构建工程，添加CGraph和workflow，准备基于workflow对外提供网络功能

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/***************************
+@Author: Chunel
+@Contact: chunel@foxmail.com
+@File: main.cpp
+@Time: 2022/4/17 22:20
+@Desc: 
+***************************/
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello, CGraph-NIO\n";
+    return 0;
+}
