b69d2dc171cc124b9502186bd737f2746b63611d
ChunelFeng <chunel@foxmail.com>
2022-04-20 00:35:12 +0800

    [add] 编译通过版本，提供编译方式

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CGraph-NIO
 基于CGraph和workflow，实现网络通信的功能
+
+## 编译说明
+
+* 本人在mac-m1机器上开发，并且本地已经安装openssl
+* 首先，按照如下方式，编译workflow，并且生成对应库文件
+```
+git clone --recursive git@github.com:CDevOrg/CGraph-NIO.git
+cd 3rd-party/workflow/
+cmake -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl -DOPENSSL_LIBRARIES=/opt/homebrew/opt/openssl/lib
+make    # 生成对应的_lib文件夹和_include文件夹
+```
+* 通过CLion打开CGraph-NIO同级的CMakeList.txt文件，即设置`OPENSSL_ROOT_DIR`为本机openssl安装位置，即可编译通过。
+
--- main.cpp
+++ main.cpp
@@ -8,15 +8,18 @@
 
 #include <iostream>
 
-#include "3rd-party/workflow/src/include/workflow/WFHttpServer.h"
+#include "3rd-party/workflow/_include/workflow/WFHttpServer.h"
 #include "3rd-party/CGraph/src/CGraph.h"
 
 using namespace CGraph;
 
 int main() {
     auto pipeline = GPipelineFactory::create();
+    CStatus status = pipeline->process();    // 假装做了啥事情
+    CGRAPH_ECHO("pipeline run ...");
+
     WFHttpServer server([](WFHttpTask *task) {
-        task->get_resp()->append_output_body("<html>Hello World!</html>");
+        task->get_resp()->append_output_body("hello, CGraph-NIO...");
     });
 
     if (server.start(8888) == 0) { // start server on port 8888
@@ -24,5 +27,6 @@ int main() {
         server.stop();
     }
 
+    GPipelineFactory::clear();
     return 0;
 }
