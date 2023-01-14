cc36e2a0b02d4afacc9b5bf5e83bba27c1f38ab1
ChunelFeng <chunel@foxmail.com>
2023-01-14 22:36:09 +0800

    更新使用逻辑

--- README.md
+++ README.md
@@ -3,13 +3,9 @@
 
 ## 编译说明
 
-* 本人在mac-m1机器上开发，并且本地已经安装openssl，安装路径为`/opt/homebrew/opt/openssl`，下方设置需要用到。
-* 首先，按照如下方式，编译workflow，并且生成对应库文件。
+* 本人在mac-m1机器上开发，并且本地已经安装openssl
+* 在下载本例前，请确保本地已经安装 workflow
 ```
 $ git clone --recursive git@github.com:CDevOrg/CGraph-NIO.git
-$ cd 3rd-party/workflow/
-$ cmake -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl -DOPENSSL_LIBRARIES=/opt/homebrew/opt/openssl/lib    # 根据实际情况，设置这两个值
-$ make    # 生成对应的_lib文件夹和_include文件夹
 ```
 * 通过CLion打开CGraph-NIO同级的CMakeList.txt文件，即设置`OPENSSL_ROOT_DIR`为本机openssl安装位置，即可编译通过。
-
--- main.cpp
+++ main.cpp
@@ -7,19 +7,58 @@
 ***************************/
 
 #include "3rd-party/CGraph/src/CGraph.h"
-#include "nio/NioInclude.h"
+#include <WFHttpServer.h>
 
 using namespace CGraph;
 
-int main() {
-    GElementPtr node = nullptr;
-    auto pipeline = GPipelineFactory::create();
-    CStatus status = pipeline->registerGElement<DWFServerNode>(&node, {}, "wf", 1);
-    status = pipeline->process();
-    if (!status.isOK()) {
-        CGRAPH_ECHO("pipeline process failed");
+static const char* PARAM_KEY = "my-param";
+struct MyParam : public GParam {
+    int loop_ = 0;
+    int val_ = 0;
+    void reset() override {
+        val_ = 0;    // 计算数值复位
+        loop_++;    // 记录轮询次数
+    }
+};
+
+struct MyNode : public GNode {
+    CStatus init() override {
+        return CGRAPH_CREATE_GPARAM(MyParam, PARAM_KEY)    // 创建参数
     }
 
-    GPipelineFactory::clear();
+    CStatus run() override {
+        auto param = CGRAPH_GET_GPARAM_WITH_NO_EMPTY(MyParam, PARAM_KEY)
+        param->val_++;    // 获取参数，并且给参数赋值
+        return CStatus();
+    }
+};
+
+void process() {
+    auto pipeline = GPipelineFactory::create();
+    GElementPtr a, b = nullptr;
+    pipeline->registerGElement<MyNode>(&a, {}, "A");
+    pipeline->registerGElement<MyNode>(&b, {a}, "B");    // 注册一个链路信息，a->b
+    pipeline->init();
+    std::mutex mtx;
+
+    WFHttpServer server([pipeline, &mtx] (WFHttpTask *task) {
+        CGRAPH_LOCK_GUARD lk(mtx);    // 防止重复进入（这里可以优化掉么？）
+        pipeline->run();    // 执行链路逻辑，参数信息是有状态的
+        auto param = pipeline->getGParamWithNoEmpty<MyParam>(PARAM_KEY);
+        task->get_resp()->append_output_body("val is : " + std::to_string(param->val_) + ", loop is " + std::to_string(param->loop_));
+    });
+
+    if (server.start(8888) == 0) {
+        getchar();
+        server.stop();
+        pipeline->destroy();
+        GPipelineFactory::clear();
+    }
+}
+
+
+int main()
+{
+    process();
     return 0;
 }
